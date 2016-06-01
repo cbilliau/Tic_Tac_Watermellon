b85614f3e59eaa64334d0b1bb0dcf9b2601cecfd
cbilliau <christopher@billiau.com>
2016-05-31 22:24:49 -0400

    Fine tuned rendering. Add reset button.

--- js/app.js
+++ js/app.js
@@ -7,25 +7,33 @@ var Counter = 0;
 var Method = {
 	determineIfTaken: function(value)	{
 		var square = Board[value];
-		console.log(square);
-		if (square !== '') {
-			alert("Square Taken");
+		if (square !== undefined) {
+			alert('Choose another square');
+			return false;
 		}
 	},
 
 	determineXO: function() {
 		Counter += 1;
+		console.log(Counter);
 		if (Counter % 2 == 0) {
-			return 'x';
-		} else {
 			return 'o';
+		} else {
+			return 'x';
 		}
 	},
 
 	assignCellVal: function(value, xOrO) {
 		Board[value] = xOrO;
-		console.log(Board[value]);
+	},
+
+	clearCellValue: function() {
+		var i;
+		for (i = 0; i <= 9; i++ ) {
+			Board[i] = '';
+		}
 	}
+
 };
 
 var View = {
@@ -44,7 +52,7 @@ var View = {
 		);
 	},
 	clearBoard: function() {
-		// loop through Board and clear all cells(1-9)
+		Method.clearCellValue();
 		Counter = 0;
 		View.renderBoard();
 	}
@@ -55,8 +63,10 @@ $(function()	{
 		var squareClicked = event.target.id;
 		// Method.determineIfTaken(squareClicked);
 		var xOrO = Method.determineXO();
-		console.log(squareClicked, xOrO);
 		Method.assignCellVal(squareClicked, xOrO);
 		View.renderBoard();
+	});
+	$('button').on('click', function() {
+		View.clearBoard();
 	});
 })
